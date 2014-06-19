6a23d74b6df7eccc142c4c82656f7b588e088b69
tjmehta <tj@init.me>
2014-06-19 02:26:41 -0400

    underscore -> camel cases correction

--- test/inflector/cases.js
+++ test/inflector/cases.js
@@ -90,11 +90,9 @@
       "Area51Controller": "area51_controller"
     },
     UnderscoreToLowerCamel: {
-      "product": "product",
-      "Widget": "widget",
-      "special_guest": "specialGuest",
-      "application_controller": "applicationController",
-      "area51_controller": "area51Controller"
+      "special-uest": "specialGuest",
+      "application-controller": "applicationController",
+      "area51-controller": "area51Controller"
     },
     DashesToLowerCamel: {
       "product": "product",
